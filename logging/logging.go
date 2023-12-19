33e3673691c95364c073da1eab846794bbbd7142
Manuel Koch <mko0815@gmail.com>
2023-12-19 21:16:33 +0100

    Add license header to files

--- logging/logging.go
+++ logging/logging.go
@@ -1,3 +1,19 @@
+// This file is part of go-auto-wlan.
+//
+// go-auto-wlan is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// go-auto-wlan is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with go-auto-wlan. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright 2023 Manuel Koch
 package logging
 
 import (
@@ -9,7 +25,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// ConfigureLoggin will setup logging
+// ConfigureLogging will setup logging
 // to log using named log level
 // to log to optional path.
 func ConfigueLogging(jsonFormat bool, logLevel string, logPath string) {
--- service/lid.go
+++ service/lid.go
@@ -1,3 +1,19 @@
+// This file is part of go-auto-wlan.
+//
+// go-auto-wlan is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// go-auto-wlan is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with go-auto-wlan. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright 2023 Manuel Koch
 package service
 
 import (
